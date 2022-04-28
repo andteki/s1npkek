cc9a77531e95f62508467f1e6f57531eb660de1d
Nagy <janos@zold.lan>
2022-04-28 08:50:31 +0200

    Adatbázishoz tartalom hozzáadva

---
+++ database/tartalom.sql
@@ -0,0 +1,8 @@
+insert into dolgozok
+(nev, telepules, fizetes)
+values
+('Tangó Béla', 'Szeged', 385),
+('Okos Árpád', 'Szolnok', 375),
+('Lehet Károly', 'Szeged', 342),
+('Terem Irén', 'Szolnok', 360),
+('Fakó Lajos', 'Szeged', 384);
