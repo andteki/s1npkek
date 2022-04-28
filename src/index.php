69505bcdae374feb74284d0babe2f49459e6141b
Nagy <janos@zold.lan>
2022-04-28 09:13:50 +0200

    Lekérés működik

--- src/index.php
+++ src/index.php
@@ -6,5 +6,20 @@ if($con->connect_error) {
     die("Hiba! A kapcsolódás sikertelen");
 }
 
-echo "ok";
+//echo "ok";
+
+$sql = <<<EOT
+select * from dolgozok
+EOT;
+
+if($result = $con->query($sql)) {
+    echo "lekérés ok";
+}else {
+    echo "Hiba! A lekérdezés sikertelen!";
+}
+
+while( $row = $result->fetch_assoc() ) {
+    echo $row['nev'];
+}
+
 $con->close();
